d587624a252f547dae7d8218e64c02c35a8c02bb
Dharam8584 <dharam_on_hunt@yahoo.in>
2024-02-24 18:09:03 +0530

    uploading data from csv file to DB

--- Udemy/DataBase/main.py
+++ Udemy/DataBase/main.py
@@ -1,17 +1,29 @@
 import _sqlite3
+import csv
 
 #name = input('Please enter the name ; ')
 #position= input('PLease enter the position')
 
-connection = _sqlite3.connect("hr.db")
-cursor = connection.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Employees
-                (Emp_iD INT, NAME TEXT, Position TEXT)''')
-employess = [(5,'Dharamkar Nitesh', 'Business'),
-            (6,'Dharamkar Dinesh', 'Cricket player'),
-            (7,'Dandotikar Apurva', 'House wife')]
+# connection = _sqlite3.connect("hr.db")
+# cursor = connection.cursor()
+# cursor.execute('''CREATE TABLE IF NOT EXISTS Employees
+#                 (Emp_iD INT, NAME TEXT, Position TEXT)''')
+# employess = [(5,'Dharamkar Nitesh', 'Business'),
+#             (6,'Dharamkar Dinesh', 'Cricket player'),
+#             (7,'Dandotikar Apurva', 'House wife')]
 
-#cursor.execute('''INSERT INTO Employees (Name,POSITION) VALUES(?,?)''',(name, position))
-cursor.executemany('''INSERT INTO Employees VALUES (?,?,?)''', employess)
+# #cursor.execute('''INSERT INTO Employees (Name,POSITION) VALUES(?,?)''',(name, position))
+# cursor.executemany('''INSERT INTO Employees VALUES (?,?,?)''', employess)
+# connection.commit()
+# connection.close()
+connection = _sqlite3.connect('Data.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS Population 
+                (City TEXT,Country TEXT,Population INT )''')
+
+file = open('population_data.csv')
+city_data = csv.reader(file)
+city_data.__next__()
+cursor.executemany(''' INSERT INTO Population VALUES (?, ? , ?)''',city_data)
 connection.commit()
-connection.close()
+connection.close
