6907f1353a34ca4cbb683933f4930eac5a8c5ed5
Dharam8584 <dharam_on_hunt@yahoo.in>
2024-02-24 18:46:41 +0530

    DB retrieve datand print on console

--- Udemy/DataBase/main.py
+++ Udemy/DataBase/main.py
@@ -21,9 +21,13 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Population 
                 (City TEXT,Country TEXT,Population INT )''')
 
-file = open('population_data.csv')
-city_data = csv.reader(file)
-city_data.__next__()
-cursor.executemany(''' INSERT INTO Population VALUES (?, ? , ?)''',city_data)
+# file = open('population_data.csv')
+# city_data = csv.reader(file)
+# city_data.__next__()
+# #cursor.executemany(''' INSERT INTO Population VALUES (?, ? , ?)''',city_data)
+results = cursor.execute('''SELECT CITY, Country FROM Population WHERE COUNTRY = "I%"''')
+for row in results:
+    print(row)
+
 connection.commit()
 connection.close
