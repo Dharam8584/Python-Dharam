5d2d0b0472b4ecf4aeb9f60c241452fc2a189f0b
Dharam8584 <dharam_on_hunt@yahoo.in>
2024-02-25 17:30:17 +0530

    commit chnages done end of this task

--- Udemy/DataBase/main.py
+++ Udemy/DataBase/main.py
@@ -1,8 +1,15 @@
 import _sqlite3
 import csv
-
-#name = input('Please enter the name ; ')
-#position= input('PLease enter the position')
+#creating Data base with joins 
+connection = _sqlite3.connect('myEmployees.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS Employees
+               (Emp_id INT , Emp_name TEXT, position TEXT , Salary INT , DEPT_ID INT)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Department
+               (DEPT_ID INT , Department_name)''')
+# country = input('Please enter the Country Name ; ')
+# city= input('PLease enter the City Name ')
+# population = input('PLease enter the population name')
 
 # connection = _sqlite3.connect("hr.db")
 # cursor = connection.cursor()
@@ -16,18 +23,19 @@ import csv
 # cursor.executemany('''INSERT INTO Employees VALUES (?,?,?)''', employess)
 # connection.commit()
 # connection.close()
-connection = _sqlite3.connect('Data.db')
-cursor = connection.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Population 
-                (City TEXT,Country TEXT,Population INT )''')
+# connection = _sqlite3.connect('Data.db')
+# cursor = connection.cursor()
+# cursor.execute('''CREATE TABLE IF NOT EXISTS Population 
+#                 (City TEXT,Country TEXT,Population INT )''')
 
 # file = open('population_data.csv')
 # city_data = csv.reader(file)
 # city_data.__next__()
 # #cursor.executemany(''' INSERT INTO Population VALUES (?, ? , ?)''',city_data)
-results = cursor.execute('''SELECT CITY, Country FROM Population WHERE COUNTRY = "I%"''')
-for row in results:
-    print(row)
+# results = cursor.execute('''SELECT CITY, Country FROM Population WHERE COUNTRY = "India"''')
+# for row in results:
+#      print(row)
+#results = cursor.execute('''INSERT INTO Population (Country, CITY, Population) VALUES (?,?,?)''',(country,city,population))
 
 connection.commit()
 connection.close
